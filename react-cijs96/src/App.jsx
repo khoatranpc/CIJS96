e789f5265b5843cdfe579fc181b0b5b396e4e72c
khotranpc <khotranpc603@gmail.com>
2024-06-05 21:20:17 +0700

    pratice btvn

--- react-cijs96/src/App.jsx
+++ react-cijs96/src/App.jsx
@@ -1,37 +1,28 @@
-import { useState } from "react";
-import PokemonCard from "./PokemonCard";
-import Modal from "./components/Modal";
-
-const data = {
-  name: "Bulbasaur",
-  img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQneX2OwCcPMNd7RkjJtUGnZRJrAR9ciOoFrw&s"
-};
+import { useState } from 'react';
+import './App.css';
 const App = () => {
-  const [pokData, setPokData] = useState(data);
-  const [crrData, setCrrData] = useState(null);
-
-  let showModal = null;
-  if (crrData) {
-    showModal = <Modal pokemon={crrData} onClose={() => {
-      setCrrData(null);
-    }} />;
+  const [light, setLight] = useState("red");
+  const handleClick = () => {
+    switch (light) {
+      case 'red':
+        setLight('orange');
+        break;
+      case 'orange':
+        setLight('green');
+        break;
+      default:
+        setLight('red');
+        break;
+    }
   }
-  /**
-   * Tư tưởng, cần phải biết được đã kích vào pokemon nào. khi nhận thấy có dữ liệu thì mới hiển thị modal
-   * còn không có thì không hiện
-   * 
-   * 
-   * - Cần phải lưu lại thông tin pokemon bất kỳ vừa kích -> dùng state
-   * -> Khi thực hiện kích pokemon -> thực hiện lưu thông tin pokemon vừa kích vào state vừa tạo
-   * -> thực hiện cài đặt logic, kiểm tra xem có dữ liệu vừa kích không
-   * nếu có -> hiển thị modal
-   */
   return (
     <div className="app">
-      <PokemonCard name={pokData.name} image={pokData.img} onClick={() => {
-        setCrrData(data);
-      }} />
-      {showModal}
+      <div className="lightTree">
+        <div className={`item ${light === 'red' ? 'red' : ''}`}></div>
+        <div className={`item ${light === 'orange' ? 'orange' : ''}`}></div>
+        <div className={`item ${light === 'green' ? 'green' : ''}`}></div>
+        <button onClick={handleClick}>Chuyển</button>
+      </div>
     </div>
   )
 }
